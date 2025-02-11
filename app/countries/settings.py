a9a0e60c617572fa392f7164cad527ef389162b4
wayne <eliutov22@gmail.com>
2025-02-11 19:28:52 +0500

    ADD env variables

--- app/countries/settings.py
+++ app/countries/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 import os.path
 from pathlib import Path
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q80$d1%aux)-ns%q03^ow-&2ygs)1$@m$e-v(eecipo6#!ha%2'
-
+# SECRET_KEY = 'django-insecure-q80$d1%aux)-ns%q03^ow-&2ygs)1$@m$e-v(eecipo6#!ha%2'
+SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ['DEBUG']
 
 ALLOWED_HOSTS = []
 
