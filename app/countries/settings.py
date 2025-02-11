76ddcc3fc13d9a4daad28e3da6b36dd3b2e4904f
wayne <eliutov22@gmail.com>
2025-02-11 21:09:26 +0500

    ADD env variables

--- app/countries/settings.py
+++ app/countries/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['95.182.99.92']
 
 
 # Application definition
